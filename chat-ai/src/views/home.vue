966629e57d25eeacae026d8cd1952a2d3160662d
HangDao311 <daohang311@gmail.com>
2023-04-17 22:26:50 +0700

    code DATN lan 1

---
+++ chat-ai/src/views/home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Hang va nhung bai hoc dau tien ve VueJs<br>
+    Thay giao huong dan: Vu Ngoc Khoa 
+
+  </div>
+</template>
+<script>
+export default {
+    name: "HomePage"
+};
+</script>
+<style lang=""></style>
---
+++ chat-ai/src/views/index.vue
@@ -0,0 +1,13 @@
+<template >
+    <div>
+        ABC1233
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
