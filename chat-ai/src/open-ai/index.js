107548fe58e533640f484d979898c3eeaee193b3
hangdao <daohang311@gmail.com>
2023-07-06 01:10:25 +0700

    update env

--- chat-ai/src/open-ai/index.js
+++ chat-ai/src/open-ai/index.js
@@ -1,6 +1,6 @@
 import { Configuration, OpenAIApi } from "openai";
 const configuration = new Configuration({
-  organization: "org-NbEojtG7YWcr324b26pjL3Ji",
+  organization: import.meta.env.VITE_OPENAI_ORGID,
   apiKey: import.meta.env.VITE_OPENAI_KEY,
 });
 const openai = new OpenAIApi(configuration);
